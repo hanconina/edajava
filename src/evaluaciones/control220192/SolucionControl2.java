af33299a839b64891d72a70993be4e8114614f00
user <user@teminal>
2019-11-04 19:31:54 -0500

    Control 2 - 2019-2

--- src/evaluaciones/control220192/SolucionControl2.java
+++ src/evaluaciones/control220192/SolucionControl2.java
@@ -5,8 +5,10 @@ import tda.pila.nodos.Pila;
 
 public class SolucionControl2 {
     public static void main(String[] args) {
+        // Pregunta 1
         int[] a = {1,4,2,3,5,7,8,1};
         System.out.println("Suma de Pares: "+sumaPares(a,a.length));
+        // Pregunta 2
         Pila p = new Pila();
         p.apilar(2);
         p.apilar(3);
@@ -14,6 +16,7 @@ public class SolucionControl2 {
         p.apilar(8);
         p.apilar(25);
         System.out.println("Numero elementos de una Pila: "+longitud(p));
+        // Pregunta 3
         p.apilar(2);
         p.apilar(3);
         p.apilar(15);
@@ -21,6 +24,10 @@ public class SolucionControl2 {
         p.apilar(25);
         System.out.println("Suma de multiplos de 5: "+sumaDivisiblesPorCinco(p));
     }
+    /*
+    PREGUNTA1.- Suma de elementos de una pila que son divisibles por 5
+    
+    */
     public static int sumaDivisiblesPorCinco(Pila p){
         int suma=0;
         while (!p.esVacia()){
@@ -32,6 +39,12 @@ public class SolucionControl2 {
         }
         return suma;
     }
+    /*
+    * Pregunta 2
+    * Sea A un arreglo unidimensional de n números enteros. 
+    * Escriba una función recursiva para determinar 
+    * la suma de los elementos pares del arreglo A.
+    */
     public static int sumaPares(int[] a,int n){
         if (n > 0){
             if (a[n-1]%2==0)
@@ -43,6 +56,12 @@ public class SolucionControl2 {
             return 0;
         }
     }
+    /*
+    * Pregunta 3
+    * Escriba una función recursiva a nivel de usuario para determinar 
+    * la cantidad de elemento de una pila. 
+    * Considere que la pila tiene los métodos apilar, desapilar y esVacia.
+    */
     public static int longitud(Pila p){
         if (p.esVacia()){
             return 0;
